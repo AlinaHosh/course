611b56d1984494bb9e509908e45139c50f56429d
AlinaHosh <73576527+AlinaHosh@users.noreply.github.com>
2022-01-27 00:45:49 +0200

    Delete index.js

--- task3/index.js
+++
@@ -1,7 +0,0 @@
-export function createPhoneNumber(numbers){
-      let part1=numbers.slice(0,3);
-      let part2=numbers.slice(3,6);
-      let part3=numbers.slice(6);
-      let num='('+part1+') '+part2+'-'+part3;
-     return num.replace(/,/g,'');;
-}
