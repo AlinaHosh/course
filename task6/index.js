20fba1b4b9b3140e2b3da7ef35766b49ca9fd1a8
Alina <alinysikigorevna@gmail.com>
2022-01-30 00:40:56 +0200

    functions were separated by different variables, boolean output, add more tests, add 'describe'

--- task6/index.js
+++ task6/index.js
@@ -1,14 +1,11 @@
 export function checkPalindrome(str){
   if(str!=null){
-    for(let i=0;i<str.length;i++){
-      if(str.charAt(i)==str.charAt(i).toUpperCase()){
-        str=str.charAt(i).toLowerCase();
-      }
-    }
-    str=str.replace(/[.*+?!,]/g,'');
-    const reverseStr=str.split('').reverse().join('').replace(/\s/g, '');
-    str=str.replace(/\s/g, '');
-    return reverseStr==str?'palindrome':'not palindrome';
-  }
-  else return 'palindrome';
-}
+     const strEdit=str.replace(/\W/g,'');
+     const strToArray=strEdit.split('');
+     const strReverse=strToArray.reverse();
+     const strToString=strReverse.join('');
+     const matches=strToString.match(/\w/g).join('');
+     return strEdit.toLowerCase()===matches.toLowerCase();
+   }
+   else return true;
+ }
--- task6/index.test.js
+++ task6/index.test.js
@@ -17,9 +17,51 @@ test('should return "not palindrome" when "banana" passed checking Palindrome',
     function(){
         const result =checkPalindrome('banana')
         expect(result).toBe('not palindrome')
-    });
-test('should return "palindrome" when string with symbol passed checking Palindrome',
-    function(){
-        const result =checkPalindrome('Was it a cat I saw?')
-        expect(result).toBe('palindrome')
-    });
+        import {checkPalindrome} from '.'
+        describe('checkPalindrome',()=>{
+        test('should return true when "Was it a cat I saw?" passed checking Palindrome',
+            function(){
+                const result =checkPalindrome('Was it a cat I saw?')
+                expect(result).toBe(true)
+            });
+        });
+        test('should return true when empty string passed checking Palindrome',
+            function(){
+                const result =checkPalindrome()
+                expect(result).toBe(true)
+            });  
+        test('should return true when "Was it a cat I saw" passed checking Palindrome',
+            function(){
+                const result =checkPalindrome('Was it a cat I saw')
+                expect(result).toBe(true)
+            });
+        test('should return false when "banana" passed checking Palindrome',
+            function(){
+                const result =checkPalindrome('banana')
+                expect(result).toBe(false)
+            });
+        test('should return false when "race a car" passed checking Palindrome',
+            function(){
+                const result =checkPalindrome('race a car')
+                expect(result).toBe(false)
+            });
+        test('should return true when "repaper" passed checking Palindrome',
+            function(){
+                const result =checkPalindrome('repaper')
+                expect(result).toBe(true)
+            });
+        test('should return true when "Eva, can I see bees in a cave?" passed checking Palindrome',
+            function(){
+                const result =checkPalindrome('Eva, can I see bees in a cave?')
+                expect(result).toBe(true)
+            });
+        test('should return true when "My age is 0, 0 si ega ym." passed checking Palindrome',
+            function(){
+                const result =checkPalindrome('My age is 0, 0 si ega ym.')
+                expect(result).toBe(true)
+            });
+        test('should return true when "000" passed checking Palindrome',
+            function(){
+                const result =checkPalindrome('000')
+                expect(result).toBe(true)
+            });
