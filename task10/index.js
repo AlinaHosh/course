d3de4672d3894d536b0998faf9108fe89e653202
Alina <alinysikigorevna@gmail.com>
2022-01-30 01:43:59 +0200

    added tests, edited function

--- task10/index.js
+++ task10/index.js
@@ -1,10 +1,14 @@
-export function alphabetPosition(text){
-  let str = text.toLowerCase();
-  let strNew = "";
-  for (let i = 0; i < str.length; i++) {
-      if (str.charCodeAt(i) - 96 > 0 && str.charCodeAt(i) - 96 <= 26) {
-        strNew+= str.charCodeAt(i) - 96 + ' ';
+export function alphabetPosition(str){
+  if(str!=null){
+    const strToLowerCase = str.toLowerCase();
+    const str1=strToLowerCase.match(/\D/gi);
+    const str2=str1.join('');
+    let strNew = "";
+    for (let i = 0; i < str1.length; i++) {
+      if(str2.charCodeAt(i) - 96 > 0 && str2.charCodeAt(i) - 96 <= 26) {
+        strNew+= str2.charCodeAt(i) - 96 + ' ';
       }
     }
     return strNew.trim();
+  }
 }
--- task10/index.test.js
+++ task10/index.test.js
@@ -1,23 +1,33 @@
 import {alphabetPosition} from '.'
-describe('vowelCount',()=>{
-test('should return "4" vowels in string',
+describe('alphabetPosition',()=>{
+test('should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" from "The sunset sets at twelve o clock." ',
     function(){
-        const result =alphabetPosition('b')
-        expect(result).toBe('2')
+        const result =alphabetPosition("The sunset sets at twelve o' clock.")
+        expect(result).toBe("20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11")
     });
 });
-test('should return "4" vowels in string',
+test('should return "1 1 2 2" from string "aabb"',
     function(){
         const result =alphabetPosition('aabb')
         expect(result).toBe('1 1 2 2')
     });
-test('should return "4" vowels in string',
+test('should return "1 1 2 2" from string "a a bb"',
     function(){
         const result =alphabetPosition('a a bb')
         expect(result).toBe('1 1 2 2')
     });
-test('should return "4" vowels in string',
+test('should return "1 1 2 2" from string "a a bb7 8"',
     function(){
         const result =alphabetPosition('a a bb7 8')
         expect(result).toBe('1 1 2 2')
+    });
+test('should return "1 1 2 2" from string "a a bb7 8"',
+    function(){
+        const result =alphabetPosition('7 3')
+        expect(result).toBe('')
+    });
+test('should return empty',
+    function(){
+        const result =alphabetPosition()
+        expect(result).toBe()
     });
