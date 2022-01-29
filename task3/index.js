031b8a8984937e489c92c826aee2def8a2c3293c
Alina <alinysikigorevna@gmail.com>
2022-01-30 00:00:37 +0200

    deleted else

--- task3/index.js
+++ task3/index.js
@@ -3,8 +3,7 @@ export function createPhoneNumber(numbers){
             const part1=numbers.slice(0,3).join('');
             const part2=numbers.slice(3,6).join('');
             const part3=numbers.slice(6).join('');
-            const num='('+part1+') '+part2+'-'+part3;
-            return num;    
-      }
-      else return 'not right number';
+            const phoneNumber='('+part1+') '+part2+'-'+part3;
+            return phoneNumber;    
+      }  
 }
--- task3/index.test.js
+++ task3/index.test.js
@@ -7,15 +7,15 @@ test('should return phone number (123) 456-7890',
         expect(result).toBe('(123) 456-7890')
     });
 });
-test('should return answer "not right answer"',
+test('should return nothing because of out of range',
     function(){
         const number=[1,2,3,4,5,6,7,8,9,0,8];
         const result =createPhoneNumber(number)
-        expect(result).toBe('not right number')
+        expect(result).toBe()
     });
-test('should return answer "not right answer"',
+test('should return nothing because of empty array of numbers',
     function(){
         const number=[];
         const result =createPhoneNumber(number)
-        expect(result).toBe('not right number')
+        expect(result).toBe()
     });
