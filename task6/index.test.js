8c6b99c3aebee23261880176d47a84f43690b36b
Alina <alinysikigorevna@gmail.com>
2022-01-30 00:42:58 +0200

    deleted unnecessary part of tests

--- task6/index.test.js
+++ task6/index.test.js
@@ -1,22 +1,3 @@
-test('should return "palindrome" when string with symbol passed checking Palindrome',
-    function(){
-        const result =checkPalindrome('Was it a cat I saw?')
-        expect(result).toBe('palindrome')
-    });
-test('should return "not palindrome" when empty string passed checking Palindrome',
-    function(){
-        const result =checkPalindrome()
-        expect(result).toBe('palindrome')
-    });  
-test('should return "palindrome" when string with upperCase and spaces passed checking Palindrome',
-    function(){
-        const result =checkPalindrome('Was it a cat I saw')
-        expect(result).toBe('palindrome')
-    });
-test('should return "not palindrome" when "banana" passed checking Palindrome',
-    function(){
-        const result =checkPalindrome('banana')
-        expect(result).toBe('not palindrome')
         import {checkPalindrome} from '.'
         describe('checkPalindrome',()=>{
         test('should return true when "Was it a cat I saw?" passed checking Palindrome',
