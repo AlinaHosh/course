51bdb91f25f4dc2bb0476ec7d846ca5645d62a2d
Alina <alinysikigorevna@gmail.com>
2022-01-30 00:17:04 +0200

    edited regular expression in function, add more tests including empty

--- task5/index.js
+++ task5/index.js
@@ -1,17 +1,17 @@
 export function getTime(str){
-    const regexp=/\d+/g;
-    const matches=str.match(regexp);
-    if(matches!=null){
-        if(matches.length%2==0){
+    if(str!=null){
+        const regexp=/\d\d\:\d\d/g;
+        const matches=str.match(regexp);       
+        if(matches!=null){
             for(let i=0;i<matches.length;i++){
-                let hour=matches[i];
-                let minute=matches[i+1];
-                if(hour<24 && minute<60){
-                    return hour+':'+minute;
+                let hour=matches[i].slice(0,2);
+                let minute=matches[i].slice(3);
+                let hourInNum=Number(hour);
+                let minuteInNum=Number(minute);
+                if(hourInNum<24 && minuteInNum<60){
+                   return hour+':'+minute;
                 }
             }
         }
-        else return '';
     }
-    else return '';
 }
--- task5/index.test.js
+++ task5/index.test.js
@@ -4,31 +4,40 @@ test('should return 09:00 time from sentence',
     function(){
         const result =getTime('Breakfast at 09:00')
         expect(result).toBe('09:00')
-      });
+    });
 });
 test('should return 21:00 from sentence',
     function(){
         const result1 =getTime('Breakfast at 09:60, Dinner at 21:00')
         expect(result1).toBe('21:00')
-      });
+    });
 test('should return 09:59 from sentence',
     function(){
         const result2 =getTime('Breakfast at 09:59, Dinner at 21:00')
         expect(result2).toBe('09:59')
-      });
+    });
 test('should return empty from sentence',
     function(){
         const result2 =getTime('Breakfast and Dinner')
-        expect(result2).toBe('')
-      });
+        expect(result2).toBe()
+    });
 test('should return empty from sentence',
     function(){
         const result2 =getTime('Breakfast 9 and Dinner')
-        expect(result2).toBe('')
-        });
-        test('should return empty from sentence',
+        expect(result2).toBe()
+    });
+test('should return empty from sentence',
     function(){
-        const arg=''
-        const result2 =getTime(arg)
-        expect(result2).toBe('')
-        });
+        const result2 =getTime()
+        expect(result2).toBe()
+    });
+test('should return empty from sentence',
+    function(){
+        const result2 =getTime('2:30')
+        expect(result2).toBe()
+    });
+test('should return empty from sentence',
+    function(){
+        const result2 =getTime('Breakfast at 9:59, Dinner at 21:00')
+        expect(result2).toBe('21:00')
+    });
