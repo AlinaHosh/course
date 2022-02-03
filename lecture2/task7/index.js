718d95f404371f61c2e161733a1cc8b46b8a710b
Alina <alinysikigorevna@gmail.com>
2022-02-03 14:11:19 +0200

    expression function, replaced case in name

--- lecture2/task7/index.js
+++ lecture2/task7/index.js
@@ -1,22 +1,24 @@
-export function invertColor(color){
-    const strToLowerCase = color.toUpperCase();
-    const arrayColor=strToLowerCase.split('')
-    let number=[]
-    const letter=['A','B','C','D','E','F']
+export let invertColor=function(color){
+    const strToUpperCase = color.toUpperCase();
+    const arrayColor=strToUpperCase.split('');
+    const number=[];
+    const letter=['A','B','C','D','E','F'];
     for(let i=0;i<10;i++){
-        number.push(i)
+        number.push(i);
     }
-    const alphabetHex=number.concat(letter)
-    let invertedColor=['#']
-    const reverseHex=alphabetHex.slice().reverse()
+    const alphabetHex=number.concat(letter);
+    const invertedColor=['#'];
+    const reverseHex=alphabetHex.slice().reverse();
     for (let i=0;i<arrayColor.length;i++){
         for(let j=0;j<alphabetHex.length;j++){       
-            if( arrayColor[i]==alphabetHex[j]){                   
-                invertedColor.push(reverseHex[j])
+            if( arrayColor[i]===alphabetHex[j]){                   
+                invertedColor.push(reverseHex[j]);
             }
         } 
     }
-    return invertedColor.join('').toLowerCase()
-}
+    return invertedColor.join('').toLowerCase();
+};
+
+
 
 
