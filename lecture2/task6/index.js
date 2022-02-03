e9afa5fb04d4c9f8208fd3e82c1f2e99acee258d
Alina <alinysikigorevna@gmail.com>
2022-02-03 14:10:03 +0200

    deleted ... in front of objects, expression function

--- lecture2/task6/index.js
+++ lecture2/task6/index.js
@@ -1,13 +1,5 @@
-export function createUrl(str,...obj){
-    let strReplaceId='';
-    let strReplaceList=''
-    if(Object.keys(obj).includes('id')){
-        strReplaceId=str.replace('{id}',obj.id)
-    }
-    if(Object.keys(obj).includes('list')){
-        strReplaceList=strReplaceId.replace('{list}',obj.list)
-    }
-    return strReplaceList
-}
-
-
+export let createUrl=function(str, objects){
+    str=str.replace('{id}',objects.id);
+    str=str.replace('{list}',objects.list);
+    return str;
+};
