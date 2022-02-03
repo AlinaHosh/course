7579ecbbcfbd99cff1080ac2a724486ae81809da
Alina <alinysikigorevna@gmail.com>
2022-02-03 14:02:32 +0200

    numbers in variable, expression function

--- lecture2/task1/index.js
+++ lecture2/task1/index.js
@@ -1,11 +1,14 @@
-export function calculateRentalCost(countOfDays){
+export let calculateRentalCost=function(countOfDays){
+  const costRent=40;
+  const offFor7Days=50;
+  const offFor3Days=20;
  if(!isNaN(countOfDays) && Math.sign(countOfDays)){
-    const rentTheCar=40*countOfDays;
+    const rentTheCar=costRent*countOfDays;
     if(countOfDays<3 && countOfDays>0)
       return rentTheCar;
       if(3<=countOfDays && countOfDays<7)
-        return rentTheCar-20;
+        return rentTheCar-offFor3Days;
       if(7<=countOfDays)
-        return rentTheCar-50;
+        return rentTheCar-offFor7Days;
  }
-}
+};
