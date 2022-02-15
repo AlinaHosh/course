20fa09b857d1b449b6fca269940ea17ddeef080c
Alina <alinysikigorevna@gmail.com>
2022-02-15 13:28:53 +0200

    arrow function, map and filter were replaced reduce, function Marh.pow replaced expression i>0, renamed variable

--- lecture2/task15/index.js
+++ lecture2/task15/index.js
@@ -1,11 +1,17 @@
-export let squareList=function(arrayNumbers){
-  const numbersFiltered=arrayNumbers.filter(function(i){   
-      if(i>0 && Number.isInteger(i)){
-        return i;
-      }
-  });
-  const square=numbersFiltered.map((num)=>num*num);
-  return square;
+export const squareList = arrayNumbers => {
+  const squareArray = arrayNumbers.reduce((total,number)=>{   
+      
+    if(Math.sign(number) === 1 && Number.isInteger(number) === true){
+      const toPow = Math.pow(number,2);
+
+      total.push(toPow);
+    }
+
+    return total;
+  },[]);
+
+  return squareArray;
 };
+
 
 
