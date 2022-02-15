b361c5852f9e0a27ff9a65e788374562f065732e
Alina <alinysikigorevna@gmail.com>
2022-02-15 13:17:55 +0200

    eslint, arrow function , condition edited

--- lecture2/task3/index.js
+++ lecture2/task3/index.js
@@ -1,12 +1,15 @@
-export let isPrime=function(number){
-  if(number>1 && !isNaN(number)){
+export const isPrime=number=>{
+
+  if(Math.sign(number)===1 && number>1 && Number.isNaN(number)===false){
     for(let i=2;i<number;i++){
       if(number%i==0){
         return false; 
       }
     }
+
     return true;
   }
+
   return false;
 };
   
