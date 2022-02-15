ebdea344b3cd83a5d70c4a5b633cb2025fee6a73
Alina <alinysikigorevna@gmail.com>
2022-02-15 13:27:02 +0200

    arrow function, eslint

--- lecture2/task14/index.js
+++ lecture2/task14/index.js
@@ -1,7 +1,10 @@
-export let frankenSplice=function(array1,array2,position){
-  const copyArray2=array2;
+export const frankenSplice = (array1,array2,position) => {
+  const copyArray2 = array2;
+
   copyArray2.splice(position,0,...array1);
+  
   return copyArray2;
 };
+
 
 
