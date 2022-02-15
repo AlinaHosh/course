5e975b3b2d6c914f67c5e4135aceac8ff2cb58a7
Alina <alinysikigorevna@gmail.com>
2022-02-15 13:31:47 +0200

    arrow, renamed variable

--- lecture2/task16/index.js
+++ lecture2/task16/index.js
@@ -1,11 +1,15 @@
-export let fearNotLetter=function(strLetters){
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+export const fearNotLetter = strLetters => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
   const firstMatchLetter = alphabet.indexOf(strLetters[0]);
-  for (let i = 0; i < strLetters.length; i++) {
-    if (strLetters[i] !== alphabet[firstMatchLetter + i]) {
-      return alphabet[firstMatchLetter + i];
+  
+  for (const letter of strLetters) {
+
+    if (strLetters[letter] !== alphabet[firstMatchLetter + letter]) {
+      
+      return alphabet[firstMatchLetter + strLetters];
     }
   }
+
   return;
 };
 
