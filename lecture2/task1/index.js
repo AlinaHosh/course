5e542464a40089f0dccf4b3200e0e929ddbf9b2a
Alina <alinysikigorevna@gmail.com>
2022-02-15 13:15:42 +0200

    eslint, values in variables, arrow function

--- lecture2/task1/index.js
+++ lecture2/task1/index.js
@@ -1,14 +1,21 @@
-export let calculateRentalCost=function(countOfDays){
-  const costRent=40;
-  const offFor7Days=50;
-  const offFor3Days=20;
- if(!isNaN(countOfDays) && Math.sign(countOfDays)){
-    const rentTheCar=costRent*countOfDays;
-    if(countOfDays<3 && countOfDays>0)
+export const calculateRentalCost=countOfDays=>{
+  const costRentEveryDay=40;
+  const off3=20;
+  const off7=50;
+
+  if(!isNaN(countOfDays) && Math.sign(countOfDays)){
+    const rentTheCar=costRentEveryDay*countOfDays;
+
+    if(countOfDays<3 && countOfDays>0){
       return rentTheCar;
-      if(3<=countOfDays && countOfDays<7)
-        return rentTheCar-offFor3Days;
-      if(7<=countOfDays)
-        return rentTheCar-offFor7Days;
- }
+    }
+       
+    if(3<=countOfDays && countOfDays<7){
+      return rentTheCar-off3;
+    }
+    
+    if(7<=countOfDays){
+      return rentTheCar-off7;
+    }
+  }
 };
