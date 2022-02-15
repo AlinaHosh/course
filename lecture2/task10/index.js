46b57b9daed4adee0b2c2e657dd178633b358591
Alina <alinysikigorevna@gmail.com>
2022-02-15 13:25:32 +0200

    arrow function,eslint,check same values

--- lecture2/task10/index.js
+++ lecture2/task10/index.js
@@ -1,6 +1,22 @@
-export let invertObject=function(object){
-  const swapped = Object.entries(object).map(([key, value]) => [value, key]);
-  return Object.fromEntries(swapped);
+export const invertObject=object=>{
+  const uniqueValues=[];
+  const values=Object.values(object);
+
+  for(const value of values){
+    if(!uniqueValues.includes(value)){
+      uniqueValues.push(value);
+    }
+  }
+
+  if(uniqueValues.length<Object.values(object).length){
+
+    return null;
+  }else{
+    const swapped = Object.entries(object).map(([key, value]) => [value, key]);
+
+    return Object.fromEntries(swapped);
+  }
 };
+
 
 
