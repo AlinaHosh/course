a70d3196cb16c2efa08a1b6d23cbf9645f69d9f7
Alina <alinysikigorevna@gmail.com>
2022-02-15 13:29:40 +0200

    edited tests

--- lecture2/task15/index.test.js
+++ lecture2/task15/index.test.js
@@ -1,18 +1,34 @@
-import {squareList} from '.'
+import {squareList} from '.';
+
 describe('squareList',()=>{
-test('should return [16, 1764, 36]',
+  test('should return [16, 1764, 36]',
     function(){
-        const result =squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2])
-        expect(result).toEqual([16, 1764, 36])
+      const result = squareList(
+        [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2],
+      );
+        
+      expect(result).toEqual(
+        [16, 1764, 36],
+      );
     });
 });
+
 test('should return [9, 100, 49]',
-    function(){
-        const result =squareList([-3.7, -5, 3, 10, 12.5, 7, -4.5, -17, 0.3])
-        expect(result).toEqual([9, 100, 49])
-    });
+  function(){
+    const result = squareList(
+      [-3.7, -5, 3, 10, 12.5, 7, -4.5, -17, 0.3],
+    );
+
+    expect(result).toEqual(
+      [9, 100, 49],
+    );
+  });
+
 test('should return []',
-    function(){
-        const result =squareList([-3.7, -5, -4.5,0])
-        expect(result).toEqual([])
-    });
+  function(){
+    const result = squareList(
+      [-3.7, -5, -4.5,0],
+    );
+        
+    expect(result).toEqual([]);
+  });
